83f6da120840a71557657f8d0fdb12fa919f8774
Samkist <samshark23451@gmail.com>
2019-09-30 10:22:15 -0400

    Updated Algorithm

--- src/me/Samkist/MagicSquares/MagicSquareGUI.java
+++ src/me/Samkist/MagicSquares/MagicSquareGUI.java
@@ -7,11 +7,17 @@ import javax.swing.*;
 public class MagicSquareGUI extends GBFrame {
 
     private static JFrame frame = new MagicSquareGUI();
-
+    private JTextField numberField = addIntegerField(0, 1, 1, 1,1 );
+    private JButton createSquare = addButton("Create Square", 2, 1, 1, 1);
 
     public static void main(String[] args) {
         frame.setTitle("Magic Squares");
         frame.setSize(400, 400);
         frame.setVisible(true);
+    }
+
+    @Override
+    public void buttonClicked(JButton jButton) {
+        SquareFactory squareFactory = new SquareFactory(Integer.parseInt(numberField.getText()));
     }
 }
--- src/me/Samkist/MagicSquares/SquareFactory.java
+++ src/me/Samkist/MagicSquares/SquareFactory.java
@@ -1,5 +1,7 @@
 package me.Samkist.MagicSquares;
 
+import javax.swing.*;
+
 public class SquareFactory {
     private int[][] square;
     private int size;
@@ -7,6 +9,7 @@ public class SquareFactory {
     public SquareFactory(int size) {
         this.size = size;
         square = new int[size][size];
+        populateArray();
     }
 
     public void populateArray() {
@@ -15,10 +18,50 @@ public class SquareFactory {
         currentY = 0;
         for(int i = 1; i <= size*size; i++) {
             square[currentX][currentY] = i;
-            if(currentY-- < 0) {
+            //Checks top
+            if(currentX++ > size-1 && currentY-- < 0) {
+                currentY++;
+            } else if(currentY-- < 0) {
+                currentX++;
+                currentY = size-1;
+            } else if(currentX++ > size-1) {
+                currentY--;
+                currentX = 0;
+            } else if(square[currentX][currentY] != 0) {
+                currentY++;
+            } else {
+                currentX++;
+                currentY--;
+            }
+/*            if(currentY-- < 0) {
+                //If top right just go down
                 if(currentX++ > size-1) {
-                    
+                    currentY++;
+                    continue;
                 }
+                //If on top row (not top right) move over one and go all the way down
+                if(!(currentX++ > size-1)) {
+                    currentY = size-1;
+                    currentX++;
+                    continue;
+                }
+            }
+            //Check if the next position over goes out of bounds, goes up one row and back
+            if(currentX++ > size-1) {
+                currentY--;
+                currentX = 0;
+                continue;
+            }
+
+            //Checks if the square diagonal is occupied
+            if(square[currentX+1][currentY+1] != 0) {
+                currentY++;
+            }*/
+        }
+
+        for(int i = 0; i < size; i++) {
+            for(int j = 0; j < size; j++) {
+                System.out.println(i + "," + j + " --> " + square[i][j]);
             }
         }
     }
