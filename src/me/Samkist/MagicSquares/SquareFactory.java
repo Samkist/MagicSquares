b1f5c9050cd6fa9987d77cfd8e6315016be960ba
Samkist <samshark23451@gmail.com>
2019-10-01 12:00:09 -0400

    Fixed increment issue on algorithm

--- src/me/Samkist/MagicSquares/SquareFactory.java
+++ src/me/Samkist/MagicSquares/SquareFactory.java
@@ -1,15 +1,33 @@
 package me.Samkist.MagicSquares;
 
+import BreezySwing.Format;
+import com.sun.javafx.collections.ElementObservableListDecorator;
+import sun.java2d.DisposerTarget;
+
 import javax.swing.*;
 
+
 public class SquareFactory {
-    private int[][] square;
+    private volatile int[][] square;
     private int size;
 
     public SquareFactory(int size) {
         this.size = size;
         square = new int[size][size];
         populateArray();
+
+    }
+
+
+
+    public String getSquareString() {
+        String squareString = "";
+        for(int i = 0; i < size; i++) {
+            for(int j = 0; j < size; j++) {
+                squareString += Format.justify('c', square[i][j], 4);
+            }
+        }
+        return squareString;
     }
 
     public void populateArray() {
@@ -19,44 +37,21 @@ public class SquareFactory {
         for(int i = 1; i <= size*size; i++) {
             square[currentX][currentY] = i;
             //Checks top
-            if(currentX++ > size-1 && currentY-- < 0) {
+            if(currentX+1 > size-1 && currentY-1 < 0) {
                 currentY++;
-            } else if(currentY-- < 0) {
+            } else if(currentY-1 < 0) {
+
                 currentX++;
                 currentY = size-1;
-            } else if(currentX++ > size-1) {
+            } else if(currentX+1 > size-1) {
                 currentY--;
                 currentX = 0;
-            } else if(square[currentX][currentY] != 0) {
+            } else if(square[currentX+1][currentY-1] != 0) {
                 currentY++;
             } else {
                 currentX++;
                 currentY--;
             }
-/*            if(currentY-- < 0) {
-                //If top right just go down
-                if(currentX++ > size-1) {
-                    currentY++;
-                    continue;
-                }
-                //If on top row (not top right) move over one and go all the way down
-                if(!(currentX++ > size-1)) {
-                    currentY = size-1;
-                    currentX++;
-                    continue;
-                }
-            }
-            //Check if the next position over goes out of bounds, goes up one row and back
-            if(currentX++ > size-1) {
-                currentY--;
-                currentX = 0;
-                continue;
-            }
-
-            //Checks if the square diagonal is occupied
-            if(square[currentX+1][currentY+1] != 0) {
-                currentY++;
-            }*/
         }
 
         for(int i = 0; i < size; i++) {
